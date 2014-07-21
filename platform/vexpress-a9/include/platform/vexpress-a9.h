f6bf6b717de93feb9b05febed0f07c12e42ecec4
Travis Geiselbrecht <geist@foobox.com>
2014-07-21 00:42:54 -0700

    [platform][vexpress-a9] add support for virtio devices

--- platform/vexpress-a9/include/platform/vexpress-a9.h
+++ platform/vexpress-a9/include/platform/vexpress-a9.h
@@ -46,10 +46,11 @@
 #define SDRAM_APERTURE_SIZE (0x40000000)
 
 /* most of the peripherals live on the motherboard CS7 */
-#define UART0_BASE  (MOTHERBOARD_CS7_VIRT + 0x9000)
-#define UART1_BASE  (MOTHERBOARD_CS7_VIRT + 0xa000)
-#define UART2_BASE  (MOTHERBOARD_CS7_VIRT + 0xb000)
-#define UART3_BASE  (MOTHERBOARD_CS7_VIRT + 0xc000)
+#define UART0_BASE  (MOTHERBOARD_CS7 + 0x9000)
+#define UART1_BASE  (MOTHERBOARD_CS7 + 0xa000)
+#define UART2_BASE  (MOTHERBOARD_CS7 + 0xb000)
+#define UART3_BASE  (MOTHERBOARD_CS7 + 0xc000)
+#define VIRTIO_BASE (MOTHERBOARD_CS7 + 0x13000)
 
 #define CPUPRIV_SIZE        (0x00100000)
 #define CPUPRIV_BASE_PHYS   (0x1e000000)
@@ -63,6 +64,10 @@
 #define UART1_INT   (32 + 6)
 #define UART2_INT   (32 + 7)
 #define UART3_INT   (32 + 8)
+#define VIRTIO0_INT (32 + 40)
+#define VIRTIO1_INT (32 + 41)
+#define VIRTIO2_INT (32 + 42)
+#define VIRTIO3_INT (32 + 43)
 
 #define MAX_INT 96
 
