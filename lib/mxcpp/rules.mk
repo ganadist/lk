3d483995901bfc3f93c93a4b05f95b3664ff9246
George Kulakowski <kulakowski@google.com>
2017-05-16 12:59:04 -0700

    [mxcpp][mxalloc] Split mxcpp into mxcpp and mxalloc
    mxcpp used to provide three things.
    
    (1) is a very minimal C++ runtime environment. This means essentially
    the definition of __cxa_pure_virtual, which is a function the compiler
    expects to have ambient. This functionality is necessary for code
    written in environments which cannot link the standard library (the
    kernel and a handful of initial userspace processes).
    
    (2) is the definitions of the standard new and delete
    overloads. Again, these are needed for code that wants to call them
    but to not link the standard library, but this time only in userspace
    (the kernel does not want certain behavior stemming from the standard
    overloads). The behavior is slightly different than the standard ones
    in failure cases, but not in a way particularly interesting to the
    rest of this email.
    
    (3) is the userspace version of our special AllocChecker new
    overloads. This mechanism exists for kernelspace, where allocation
    failure is real and needs to be handled. This mechanism works
    perfectly well in userspace, in that it compiles, even though the
    underlying allocator is unlikely to ever return nullptr. It's
    therefore essentially harmless in userspace. A handful of small
    libraries are used in both userspace and in the kernel, and also use
    this mechanism. Note that this feature is essentially copy-pasted
    between the kernel and userspace.
    
    This all works perfectly well for the kernel.
    
    Some userspace code also uses these mechanisms, as mentioned. As long
    as the standard library isn't linked, there are again no issues.
    
    But as soon as you want both (3) and std::vector, which is a
    reasonable thing to want!, there are issues because (1) and (3) come
    in the same static library. This causes link failures due to duplicate
    __cxa_pure_virtual symbols. This issue is exacerbated by different
    behavior in Linux, OS X, and target linkage, in terms of confusion and
    it-works-on-my-machine sort of issues.
    
    This patch splits mxcpp into: mxcpp, containing (1) __cxa_pure_virtual
    and (2) only if in userspace, the standard new and delete overloads;
    and mxalloc, containing (3) AllocChecker. mxcpp is mutually exclusive
    of the standard library. mxalloc can be linked against both code using
    the standard library, and against code using mxcpp.
    
    This patch also deduplicates the AllocChecker between userspace and
    kernel, and moves the InlineArray class into the kernel (its only
    callsites are in the kernel).
    
    Change-Id: Id42d5d019ee977d44d6cb89ce7ab6884f7a56cd1

--- lib/mxcpp/rules.mk
+++ lib/mxcpp/rules.mk
@@ -1,20 +1,18 @@
-# Copyright 2016 The Fuchsia Authors
-# Copyright (c) 2008-2015 Travis Geiselbrecht
+# Copyright 2017 The Fuchsia Authors
 #
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT
 
-SRC_DIR := system/ulib/bitmap
+SRC_DIR := system/ulib/mxcpp
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
 
-KERNEL_INCLUDES += $(SRC_DIR)/include
-
-MODULE_DEPS := kernel/lib/mxalloc
-
 MODULE_SRCS := \
-    $(SRC_DIR)/rle-bitmap.cpp
+    $(SRC_DIR)/new.cpp \
+    $(SRC_DIR)/pure_virtual.cpp \
+
+KERNEL_INCLUDES += $(SRC_DIR)/include
 
 include make/module.mk
