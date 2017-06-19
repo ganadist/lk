litterbug23/magenta

[vm] Implement VmAddressRegionDispatcher
VmAddressRegionDispatcher will back the handles used by the
soon-to-come new virtual memory syscall interface.

Change-Id: I929f9d87d85155a335c7dcc5d225d22f885e3d5c

kernel/lib/magenta/diagnostics.cpp
@@ -23,7 +23,8 @@ void DumpProcessListKeyMap() {
     printf("#jb : number of job handles\n");
     printf("#pr : number of process handles\n");
     printf("#th : number of thread handles\n");
-    printf("#vm : number of vm map handles\n");
+    printf("#vo : number of vmo handles\n");
+    printf("#vm : number of virtual memory address region handles\n");
     printf("#mp : number of message pipe handles\n");
     printf("#ev : number of event and event pair handles\n");
     printf("#ip : number of io port handles\n");
@@ -45,7 +46,7 @@ char StateChar(const ProcessDispatcher& pd) {
 }
 
 const char* ObjectTypeToString(mx_obj_type_t type) {
-    static_assert(MX_OBJ_TYPE_LAST == 18, "need to update switch below");
+    static_assert(MX_OBJ_TYPE_LAST == 19, "need to update switch below");
 
     switch (type) {
         case MX_OBJ_TYPE_PROCESS: return "process";
@@ -65,6 +66,7 @@ const char* ObjectTypeToString(mx_obj_type_t type) {
         case MX_OBJ_TYPE_RESOURCE: return "resource";
         case MX_OBJ_TYPE_EVENT_PAIR: return "event-pair";
         case MX_OBJ_TYPE_JOB: return "job";
+        case MX_OBJ_TYPE_VMAR: return "vmar";
         default: return "???";
     }
 }
@@ -98,12 +100,13 @@ char* DumpHandleTypeCount_NoLock(const ProcessDispatcher& pd) {
     uint32_t types[MX_OBJ_TYPE_LAST] = {0};
     uint32_t handle_count = BuildHandleStats(pd, types, sizeof(types));
 
-    snprintf(buf, sizeof(buf), "%3u: %3u %3u %3u %3u %3u %3u %3u %3u",
+    snprintf(buf, sizeof(buf), "%3u: %3u %3u %3u %3u %3u %3u %3u %3u %3u",
              handle_count,
              types[MX_OBJ_TYPE_JOB],
              types[MX_OBJ_TYPE_PROCESS],
              types[MX_OBJ_TYPE_THREAD],
              types[MX_OBJ_TYPE_VMEM],
+             types[MX_OBJ_TYPE_VMAR],
              types[MX_OBJ_TYPE_CHANNEL],
              // Events and event pairs:
              types[MX_OBJ_TYPE_EVENT] + types[MX_OBJ_TYPE_EVENT_PAIR],
@@ -116,7 +119,7 @@ char* DumpHandleTypeCount_NoLock(const ProcessDispatcher& pd) {
 
 void DumpProcessList() {
     AutoLock lock(& ProcessDispatcher::global_process_list_mutex_);
-    printf("%8s-s  #t  #pg  #h:  #jb #pr #th #vm #mp #ev #ip #dp [  job:name]\n", "id");
+    printf("%8s-s  #t  #pg  #h:  #jb #pr #th #vo #vm #mp #ev #ip #dp [  job:name]\n", "id");
 
     for (const auto& process : ProcessDispatcher::global_process_list_) {
         printf("%8" PRIu64 "-%c %3u %4zu %s  [%5" PRIu64 ":%s]\n",

kernel/lib/magenta/vm_address_region_dispatcher.cpp
@@ -1,0 +1,144 @@
+// Copyright 2016 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <magenta/vm_address_region_dispatcher.h>
+
+#include <kernel/vm/vm_address_region.h>
+#include <kernel/vm/vm_aspace.h>
+#include <kernel/vm/vm_object.h>
+
+#include <assert.h>
+#include <new.h>
+#include <err.h>
+#include <inttypes.h>
+#include <trace.h>
+
+#define LOCAL_TRACE 0
+
+constexpr mx_rights_t kDefaultVmarRights =
+    MX_RIGHT_DUPLICATE | MX_RIGHT_TRANSFER;
+
+status_t VmAddressRegionDispatcher::Create(mxtl::RefPtr<VmAddressRegion> vmar,
+                                    mxtl::RefPtr<Dispatcher>* dispatcher,
+                                    mx_rights_t* rights) {
+
+    // The initial rights should match the VMAR's creation permissions
+    mx_rights_t vmar_rights = kDefaultVmarRights;
+    uint32_t vmar_flags = vmar->flags();
+    if (vmar_flags & VMAR_FLAG_CAN_MAP_READ) {
+        vmar_rights |= MX_RIGHT_READ;
+    }
+    if (vmar_flags & VMAR_FLAG_CAN_MAP_WRITE) {
+        vmar_rights |= MX_RIGHT_WRITE;
+    }
+    if (vmar_flags & VMAR_FLAG_CAN_MAP_EXEC) {
+        vmar_rights |= MX_RIGHT_EXECUTE;
+    }
+
+    AllocChecker ac;
+    auto disp = new (&ac) VmAddressRegionDispatcher(mxtl::move(vmar));
+    if (!ac.check())
+        return ERR_NO_MEMORY;
+
+    *rights = vmar_rights;
+    *dispatcher = mxtl::AdoptRef<Dispatcher>(disp);
+    return NO_ERROR;
+}
+
+VmAddressRegionDispatcher::VmAddressRegionDispatcher(mxtl::RefPtr<VmAddressRegion> vmar)
+    : vmar_(vmar) {}
+
+VmAddressRegionDispatcher::~VmAddressRegionDispatcher() {}
+
+mx_status_t VmAddressRegionDispatcher::Allocate(
+        size_t offset, size_t size, uint32_t flags, mxtl::RefPtr<VmAddressRegion>* out) {
+
+    return vmar_->CreateSubVmar(offset, size, /* align_pow2 */ 0 , flags, "useralloc", out);
+}
+
+mx_status_t VmAddressRegionDispatcher::Destroy() {
+    return vmar_->Destroy();
+}
+
+mx_status_t VmAddressRegionDispatcher::Map(size_t vmar_offset, mxtl::RefPtr<VmObject> vmo,
+                                           uint64_t vmo_offset, size_t len, uint32_t vmar_flags,
+                                           uint arch_mmu_flags, mxtl::RefPtr<VmMapping>* out) {
+
+    mxtl::RefPtr<VmMapping> result(nullptr);
+
+    status_t status = vmar_->CreateVmMapping(vmar_offset, len, /* align_pow2 */ 0,
+                                             vmar_flags, mxtl::move(vmo), vmo_offset,
+                                             arch_mmu_flags, "useralloc",
+                                             &result);
+    if (status != NO_ERROR) {
+        return status;
+    }
+
+    *out = mxtl::move(result);
+    return NO_ERROR;
+}
+
+mx_status_t VmAddressRegionDispatcher::Protect(size_t offset, size_t len, uint arch_mmu_flags) {
+    // TODO(teisenbe): Consider supporting splitting mappings; it's unclear if
+    // there's a usecase for that versus just creating multiple mappings to
+    // start with.
+
+    mxtl::RefPtr<VmMapping> mapping(nullptr);
+    {
+        mxtl::RefPtr<VmAddressRegionOrMapping> child = vmar_->FindRegion(vmar_->base() + offset);
+        if (!child) {
+            return ERR_NOT_FOUND;
+        }
+        mapping = child->as_vm_mapping();
+    }
+
+    if (!mapping) {
+        return ERR_NOT_FOUND;
+    }
+
+    // For now, require that the request be for an entire VmMapping.
+    // Additionally, special case len=0 to mean the whole region.
+    // TODO(teisenbe): Remove this
+    DEBUG_ASSERT(mapping->base() >= vmar_->base());
+    size_t mapping_offset = mapping->base() - vmar_->base();
+    if (len != 0) {
+        if (mapping_offset != offset || mapping->size() != len) {
+            return ERR_INVALID_ARGS;
+        }
+    }
+
+    return mapping->Protect(arch_mmu_flags);
+}
+
+mx_status_t VmAddressRegionDispatcher::Unmap(size_t offset, size_t len) {
+    // TODO(teisenbe): This should actually allow spanning multiple regions.
+    mxtl::RefPtr<VmMapping> mapping(nullptr);
+    {
+        mxtl::RefPtr<VmAddressRegionOrMapping> child = vmar_->FindRegion(vmar_->base() + offset);
+        if (!child) {
+            return ERR_NOT_FOUND;
+        }
+        mapping = child->as_vm_mapping();
+    }
+
+    if (!mapping) {
+        return ERR_NOT_FOUND;
+    }
+
+    // For now, require that the request be for an entire
+    // VmMapping/VmAddressRegion, and if len=0, interpret as a request
+    // to unmap the entire entry that contains the offset
+    // TODO(teisenbe): This should actually allow spanning multiple regions.
+    DEBUG_ASSERT(mapping->base() >= vmar_->base());
+    size_t mapping_offset = mapping->base() - vmar_->base();
+    if (len != 0) {
+        if (mapping_offset != offset || mapping->size() != len) {
+            return ERR_INVALID_ARGS;
+        }
+    }
+
+    return mapping->Unmap();
+}

ganadist/lk

Change-Id: I4cf54145d5e404b511576b242cc4135dad955062

lib/magenta/diagnostics.cpp
@@ -24,7 +24,8 @@ static void DumpProcessListKeyMap() {
     printf("#jb : number of job handles\n");
     printf("#pr : number of process handles\n");
     printf("#th : number of thread handles\n");
-    printf("#vm : number of vm map handles\n");
+    printf("#vo : number of vmo handles\n");
+    printf("#vm : number of virtual memory address region handles\n");
     printf("#ch : number of channel handles\n");
     printf("#ev : number of event and event pair handles\n");
     printf("#ip : number of io port handles\n");
@@ -45,7 +46,7 @@ static char StateChar(const ProcessDispatcher& pd) {
 }
 
 static const char* ObjectTypeToString(mx_obj_type_t type) {
-    static_assert(MX_OBJ_TYPE_LAST == 18, "need to update switch below");
+    static_assert(MX_OBJ_TYPE_LAST == 19, "need to update switch below");
 
     switch (type) {
         case MX_OBJ_TYPE_PROCESS: return "process";
@@ -63,6 +64,7 @@ static const char* ObjectTypeToString(mx_obj_type_t type) {
         case MX_OBJ_TYPE_RESOURCE: return "resource";
         case MX_OBJ_TYPE_EVENT_PAIR: return "event-pair";
         case MX_OBJ_TYPE_JOB: return "job";
+        case MX_OBJ_TYPE_VMAR: return "vmar";
         default: return "???";
     }
 }
@@ -96,12 +98,13 @@ static char* DumpHandleTypeCount_NoLock(const ProcessDispatcher& pd) {
     uint32_t types[MX_OBJ_TYPE_LAST] = {0};
     uint32_t handle_count = BuildHandleStats(pd, types, sizeof(types));
 
-    snprintf(buf, sizeof(buf), "%3u: %3u %3u %3u %3u %3u %3u %3u",
+    snprintf(buf, sizeof(buf), "%3u: %3u %3u %3u %3u %3u %3u %3u %3u",
              handle_count,
              types[MX_OBJ_TYPE_JOB],
              types[MX_OBJ_TYPE_PROCESS],
              types[MX_OBJ_TYPE_THREAD],
              types[MX_OBJ_TYPE_VMEM],
+             types[MX_OBJ_TYPE_VMAR],
              types[MX_OBJ_TYPE_CHANNEL],
              // Events and event pairs:
              types[MX_OBJ_TYPE_EVENT] + types[MX_OBJ_TYPE_EVENT_PAIR],
@@ -112,7 +115,7 @@ static char* DumpHandleTypeCount_NoLock(const ProcessDispatcher& pd) {
 
 void DumpProcessList() {
     AutoLock lock(& ProcessDispatcher::global_process_list_mutex_);
-    printf("%8s-s  #t  #pg  #h:  #jb #pr #th #vm #ch #ev #ip #dp [  job:name]\n", "id");
+    printf("%8s-s  #t  #pg  #h:  #jb #pr #th #vo #vm #ch #ev #ip #dp [  job:name]\n", "id");
 
     for (const auto& process : ProcessDispatcher::global_process_list_) {
         char pname[MX_MAX_NAME_LEN];

lib/magenta/vm_address_region_dispatcher.cpp
@@ -1,0 +1,144 @@
+// Copyright 2016 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#include <magenta/vm_address_region_dispatcher.h>
+
+#include <kernel/vm/vm_address_region.h>
+#include <kernel/vm/vm_aspace.h>
+#include <kernel/vm/vm_object.h>
+
+#include <assert.h>
+#include <new.h>
+#include <err.h>
+#include <inttypes.h>
+#include <trace.h>
+
+#define LOCAL_TRACE 0
+
+constexpr mx_rights_t kDefaultVmarRights =
+    MX_RIGHT_DUPLICATE | MX_RIGHT_TRANSFER;
+
+status_t VmAddressRegionDispatcher::Create(mxtl::RefPtr<VmAddressRegion> vmar,
+                                    mxtl::RefPtr<Dispatcher>* dispatcher,
+                                    mx_rights_t* rights) {
+
+    // The initial rights should match the VMAR's creation permissions
+    mx_rights_t vmar_rights = kDefaultVmarRights;
+    uint32_t vmar_flags = vmar->flags();
+    if (vmar_flags & VMAR_FLAG_CAN_MAP_READ) {
+        vmar_rights |= MX_RIGHT_READ;
+    }
+    if (vmar_flags & VMAR_FLAG_CAN_MAP_WRITE) {
+        vmar_rights |= MX_RIGHT_WRITE;
+    }
+    if (vmar_flags & VMAR_FLAG_CAN_MAP_EXECUTE) {
+        vmar_rights |= MX_RIGHT_EXECUTE;
+    }
+
+    AllocChecker ac;
+    auto disp = new (&ac) VmAddressRegionDispatcher(mxtl::move(vmar));
+    if (!ac.check())
+        return ERR_NO_MEMORY;
+
+    *rights = vmar_rights;
+    *dispatcher = mxtl::AdoptRef<Dispatcher>(disp);
+    return NO_ERROR;
+}
+
+VmAddressRegionDispatcher::VmAddressRegionDispatcher(mxtl::RefPtr<VmAddressRegion> vmar)
+    : vmar_(mxtl::move(vmar)) {}
+
+VmAddressRegionDispatcher::~VmAddressRegionDispatcher() {}
+
+mx_status_t VmAddressRegionDispatcher::Allocate(
+        size_t offset, size_t size, uint32_t flags, mxtl::RefPtr<VmAddressRegion>* out) {
+
+    return vmar_->CreateSubVmar(offset, size, /* align_pow2 */ 0 , flags, "useralloc", out);
+}
+
+mx_status_t VmAddressRegionDispatcher::Destroy() {
+    return vmar_->Destroy();
+}
+
+mx_status_t VmAddressRegionDispatcher::Map(size_t vmar_offset, mxtl::RefPtr<VmObject> vmo,
+                                           uint64_t vmo_offset, size_t len, uint32_t vmar_flags,
+                                           uint arch_mmu_flags, mxtl::RefPtr<VmMapping>* out) {
+
+    mxtl::RefPtr<VmMapping> result(nullptr);
+
+    status_t status = vmar_->CreateVmMapping(vmar_offset, len, /* align_pow2 */ 0,
+                                             vmar_flags, mxtl::move(vmo), vmo_offset,
+                                             arch_mmu_flags, "useralloc",
+                                             &result);
+    if (status != NO_ERROR) {
+        return status;
+    }
+
+    *out = mxtl::move(result);
+    return NO_ERROR;
+}
+
+mx_status_t VmAddressRegionDispatcher::Protect(size_t offset, size_t len, uint arch_mmu_flags) {
+    // TODO(teisenbe): Consider supporting splitting mappings; it's unclear if
+    // there's a usecase for that versus just creating multiple mappings to
+    // start with.
+
+    mxtl::RefPtr<VmMapping> mapping(nullptr);
+    {
+        mxtl::RefPtr<VmAddressRegionOrMapping> child = vmar_->FindRegion(vmar_->base() + offset);
+        if (!child) {
+            return ERR_NOT_FOUND;
+        }
+        mapping = child->as_vm_mapping();
+    }
+
+    if (!mapping) {
+        return ERR_NOT_FOUND;
+    }
+
+    // For now, require that the request be for an entire VmMapping.
+    // Additionally, special case len=0 to mean the whole region.
+    // TODO(teisenbe): Remove this
+    DEBUG_ASSERT(mapping->base() >= vmar_->base());
+    size_t mapping_offset = mapping->base() - vmar_->base();
+    if (len != 0) {
+        if (mapping_offset != offset || mapping->size() != len) {
+            return ERR_INVALID_ARGS;
+        }
+    }
+
+    return mapping->Protect(arch_mmu_flags);
+}
+
+mx_status_t VmAddressRegionDispatcher::Unmap(size_t offset, size_t len) {
+    // TODO(teisenbe): This should actually allow spanning multiple regions.
+    mxtl::RefPtr<VmMapping> mapping(nullptr);
+    {
+        mxtl::RefPtr<VmAddressRegionOrMapping> child = vmar_->FindRegion(vmar_->base() + offset);
+        if (!child) {
+            return ERR_NOT_FOUND;
+        }
+        mapping = child->as_vm_mapping();
+    }
+
+    if (!mapping) {
+        return ERR_NOT_FOUND;
+    }
+
+    // For now, require that the request be for an entire
+    // VmMapping/VmAddressRegion, and if len=0, interpret as a request
+    // to unmap the entire entry that contains the offset
+    // TODO(teisenbe): Remove this
+    DEBUG_ASSERT(mapping->base() >= vmar_->base());
+    size_t mapping_offset = mapping->base() - vmar_->base();
+    if (len != 0) {
+        if (mapping_offset != offset || mapping->size() != len) {
+            return ERR_INVALID_ARGS;
+        }
+    }
+
+    return mapping->Unmap();
+}